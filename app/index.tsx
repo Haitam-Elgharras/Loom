e96985b565929f4297bd3050a70ca70289d34bcc
haitam-elgharras <elgharras.haitam@gmail.com>
2025-01-21 12:01:33 +0100

    refactor: remove unused hooks and components, add constants for icons and images, and update Tailwind configuration

--- app/index.tsx
+++ app/index.tsx
@@ -5,7 +5,7 @@ import { Text, View } from "react-native";
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text>Aora</Text>
+      <Text className="text-3xl font-pblack">Loom!</Text>
       <StatusBar style="auto" />
       <Link href="/profile" style={{ color: "blue" }}>
         Go to profile
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./<custom directory>/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#161622",
+      },
+    },
   },
   plugins: [],
 };
